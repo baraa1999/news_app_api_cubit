ace3c9d81650e8c9f7470437ee159b57d6f01749
baraa1999 <baraaalian7@gmail.com>
2023-07-21 23:43:52 +0300

    Imepelment the search screen top widgets

--- lib/inner_screens/search_screen.dart
+++ lib/inner_screens/search_screen.dart
@@ -1,16 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:news_app_api_cubit/services/utils.dart';
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  late final TextEditingController _searchTextController;
+  late final FocusNode focusNode;
+  @override
+  void initState() {
+    super.initState();
+    _searchTextController = TextEditingController();
+    focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    if (mounted) {
+      _searchTextController.dispose();
+      focusNode.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text(
-        'search',
-        style: TextStyle(fontSize: 50),
-      )),
+    Size size = Utils(context).getScreenSize;
+    final Color color = Utils(context).getColor;
+    return SafeArea(
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        focusNode.unfocus();
+                        Navigator.pop(context);
+                      },
+                      child: const Icon(
+                        IconlyLight.arrowLeft2,
+                      ),
+                    ),
+                    Flexible(
+                      child: TextField(
+                        focusNode: focusNode,
+                        controller: _searchTextController,
+                        style: TextStyle(color: color),
+                        autofocus: true,
+                        textInputAction: TextInputAction.search,
+                        keyboardType: TextInputType.text,
+                        onEditingComplete: () {},
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.only(bottom: 8 / 5),
+                          hintText: "search",
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          suffix: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: GestureDetector(
+                              onTap: () {
+                                _searchTextController.clear();
+                                focusNode.unfocus;
+                                setState(() {});
+                              },
+                              child: const Icon(
+                                Icons.close,
+                                size: 18,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
